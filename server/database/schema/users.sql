66db226f959a95e385b298a17096ef293d34c807
Stephen Stafford <srstafford1996@gmail.com>
2017-04-24 08:27:38 -0400

    User schema added to intialization

---
+++ server/database/schema/users.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Users_TC (
+    accountName VARCHAR(26) PRIMARY KEY NOT NULL,
+    hashedPass BINARY(60) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    displayName VARCHAR(26) NOT NULL
+) CHARACTER SET utf8mb4;
+
+CREATE TABLE Permissions_TC (
+    id INT(31) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+) CHARACTER SET utf8mb4;
+
+#Many<->Many Lookup Table for Permissions + Users
+CREATE TABLE Permissions_Users_TC (
+    permissionId INT(31) UNSIGNED,
+    userAccountName VARCHAR(26),
+    FOREIGN KEY(permissionId) REFERENCES Permissions_TC(id),
+    FOREIGN KEY(userAccountName) REFERENCES Users_TC(accountName)
+) CHARACTER SET utf8mb4;
+
