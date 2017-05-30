f7cfae694011064aa8621c4146399f2c55fdcc40
Stephen Stafford <stafford.stephen@ibm.com>
2017-05-30 08:54:31 -0400

    Updated unit test

--- test/test.js
+++ test/test.js
@@ -220,7 +220,7 @@ describe('Character Class', function() {
         it('should return formatted MySQL query based on object data', function() {
             let expectedQuery = new RegExp(`INSERT INTO ${CHARACTERS_TABLE_NAME} \\(name, label, game, last_updated\\) VALUES \\(\\'${testData.metadata.name}\\', \\'${testData.metadata.character}\\', \\'${testData.metadata.game}\\', [0-9]+\\)`);
             return testCharacter.buildInsertQuery().then((query) => {
-                assert( query.match(expectedQuery) );
+                assert( query.match(expectedQuery), 'We expect the returned query to match our format.');
             });
         });
     });
@@ -250,8 +250,22 @@ describe('Character Class', function() {
         let attacksTestCharacter = new Character(attacksTestData);
 
         it('should return formatted MySQL query based on data in moves array', function() {
-            let attacks = attacksTestData.moves[0];
-            let expectedQuery = 'INSERT INTO ${ATTACKS_TABLE_NAME} (character_id, notation, hit_level, damage, speed, on_block, on_hit, on_ch, notes, properties, attack_num) VALUES (${attacksTestData.metadata.id}, ${attack.notation,} ${attack.hit_level}, ${attack.damage}, ${attack.speed}, ${attack.on_block}, ${attack.on_hit}, ${attack.on_ch}, ${attack.notes}, ${attack.properties}, ${attack.attack_num})';
+            let attack = attacksTestData.moves[0];
+            let expectedQuery = mysql.format(`INSERT INTO ${ATTACKS_TABLE_NAME} (character_id, notation, hit_level, damage, speed, on_block, on_hit, on_ch, notes, properties, attack_num) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, [attacksTestData.metadata.id, attack.notation, attack.hit_level, attack.damage, attack.speed, attack.on_block, attack.on_hit, attack.on_ch, attack.notes, '', 1]);
+            return attacksTestCharacter.buildInsertQueryForAttacks().then((query) => {
+                assert.equal(query, expectedQuery, 'We expect the returned query to match.' );
+            });
+        });
+    });
+
+    describe('#toJSON()', function() {
+        it('should return character in JSON form', function() {
+            return testCharacter.toJSON().then((json) => {
+                //Clone hack
+                let expectedFormat = JSON.parse( JSON.stringify(testData) );
+                expectedFormat.moves[0] = Object.assign(expectedFormat.moves[0], {properties: [], attack_num: null, character_id: null});
+                assert.deepEqual(json, expectedFormat, 'We expect the returned JSON data to match our format.');
+            });
         });
     });
 });
